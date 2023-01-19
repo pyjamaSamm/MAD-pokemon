0966c9f801c1c49ce0048c7f86cf54bc9ec73605
pyjamaSamm <74822950+pyjamaSamm@users.noreply.github.com>
2023-01-19 22:09:43 +0530

    create Pokemon.js

---
+++ pokemon.js
@@ -0,0 +1,13 @@
+
+const fetchPokemonData = async () => {
+    const pokemonDetails = await fetch(`https://pokeapi.co/api/v2/pokemon?`);
+    const data = await pokemonDetails.json()
+    let pokemonData = data.results;
+    if (pokemonData) {
+        for (let i = 0; i < pokemonData.length; i++)
+            document.write("<li><b>Name: </b>" + pokemonData[i].name + ` <a href=${pokemonData[i].url}>` + "link" + "</a></li>")
+    }
+    console.log(data.results);
+};
+
+fetchPokemonData()
